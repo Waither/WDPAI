c499e8b60b3586699ce4331e4a46e5b622f7fc81
Maciej Gąsior <maciek550@interia.eu>
2025-03-08 19:58:03 +0100

    setup more files for the project

--- index.php
+++ index.php
@@ -1,55 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>TruckStopInfo</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+// preg_match('/mobile|android|iphone|ipad|phone/i', $userAgent ?? $_SERVER['HTTP_USER_AGENT']) ? 'true' : 'false';
 
-    <!-- Logo -->
-    <link rel="icon" href="/src/img/logo.png" type="image/x-icon">
+$request = $_SERVER['REQUEST_URI'];
+$viewDir = '/public/views/';
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="/src/css/main.css">
+$routes = [
+    '/' => 'home.html',
+    '/places' => 'places.html',
+    '/map' => 'map.html',
+    '/'
+];
 
-    <!-- JavaScript -->
-    <script src="/src/js/main.js" type="module"></script>
-</head>
-<body style="height: 100%;">
-    <script>
-        console.log(<?= preg_match('/mobile|android|iphone|ipad|phone/i', $userAgent ?? $_SERVER['HTTP_USER_AGENT']) ? 'true' : 'false' ?> ? 'Użytkownik korzysta z urządzenia mobilnego.' : 'Użytkownik korzysta z komputera.');
-    </script>
-    TruckStopInfo
-    <!--
-        Jak wygląda User-Agent w różnych przeglądarkach?
+$page = $routes[$request] ?? '404.html';
+if (!file_exists(__DIR__ . "{$viewDir}{$page}")) {
+    $page = '501.html';
+}
 
-        Przykładowe wartości $_SERVER['HTTP_USER_AGENT'] dla różnych przeglądarek:
-        Chrome (Windows 10)
-
-        Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36
-
-        Chrome (Android)
-
-        Mozilla/5.0 (Linux; Android 13; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36
-
-        Firefox (Windows 10)
-
-        Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0
-
-        Firefox (Android)
-
-        Mozilla/5.0 (Android 13; Mobile; rv:123.0) Gecko/123.0 Firefox/123.0
-
-        Edge (Windows 10)
-
-        Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0
-
-        Safari (Mac)
-
-        Mozilla/5.0 (Macintosh; Intel Mac OS X 13_3) AppleWebKit/537.36 (KHTML, like Gecko) Version/16.3 Safari/537.36
-
-        Safari (iPhone)
-
-        Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/537.36 
-    -->
-</body>
-</html>
+require __DIR__ . "{$viewDir}{$page}";
